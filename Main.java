55a2836a021561ff9515c6c859a521ae81f65979
nuoriceo <144800127+nuoriceo@users.noreply.github.com>
2023-09-28 22:31:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String [] args) {
+        String taulukko [] = {"Tupu", "Hupu", "Lupu", "Hessu Hopo", "Aku Ankka"};
+        for (int i = 0; i < taulukko.length; i += 2) {
+            System.out.println(taulukko[i]);
+        } 
+    }
+}
