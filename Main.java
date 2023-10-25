d6f9ac884753cbd64db61d4e656d6d98e941b461
Arvo Karaksela <144800127+ArvoKaraksela@users.noreply.github.com>
2023-10-25 21:48:07 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String [] args) {
-        String taulukko [] = {"Tupu", "Hupu", "Lupu", "Hessu Hopo", "Aku Ankka"};
-        for (int i = 0; i < taulukko.length; i += 2) {
-            System.out.println(taulukko[i]);
-        } 
-    }
-}
